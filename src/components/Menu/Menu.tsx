284a0b3f9d85857d4d49521752a3e95977299c14
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2019-09-04 21:49:10 +0500

    Menu Update

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -15,103 +15,114 @@ export default class Menu extends Component {
     return (
       <div className="Menu">
         <div
-          className="Menu--button"
+          className={
+            active ? "Menu__button Menu__button--active" : "Menu__button"
+          }
           onClick={() => this.setState({ active: !active })}
         >
+          <div
+            className={
+              active ? "Menu__label Menu__label--active" : "Menu__label"
+            }
+          >
+            Menu
+          </div>
           <MenuButton menuState={active} />
         </div>
 
         <div
           className={
-            active ? "Menu--overlay Menu--overlay-active" : "Menu--overlay"
+            active ? "Menu__overlay Menu__overlay--active" : "Menu__overlay"
           }
         >
           <div
             className={
-              active ? "Menu--branding Menu--branding-active" : "Menu--branding"
+              active
+                ? "Menu__branding Menu__branding--active"
+                : "Menu__branding"
             }
           >
-            <h1 className="Menu--logo">
-              Asjad <span className="Menu--text-secondaryColor">Jawed</span>
+            <h1 className="Menu__logo">
+              Asjad <span className="Menu__text--secondaryColor">Jawed</span>
             </h1>
 
             <div>
               <a
-                className="Menu--iconLink"
+                className="Menu__iconLink"
                 href="https://twitter.com/asjadjawed"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <i className="Menu--icon fab fa-twitter" />
+                <i className="Menu__icon fab fa-twitter" />
               </a>
 
               <a
-                className="Menu--iconLink"
+                className="Menu__iconLink"
                 href="https://www.linkedin.com/in/asjadjawed"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <i className="Menu--icon fab fa-linkedin-in" />
+                <i className="Menu__icon fab fa-linkedin-in" />
               </a>
 
               <a
-                className="Menu--iconLink"
+                className="Menu__iconLink"
                 href="https://github.com/asjadjawed"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <i className="Menu--icon fab fa-github" />
+                <i className="Menu__icon fab fa-github" />
               </a>
             </div>
           </div>
-          <div className={active ? "Menu--nav Menu--nav-active" : "Menu--nav"}>
-            <ul className="Menu--ul">
-              <li className={active ? "Menu--li Menu--li-active" : "Menu--li"}>
+          <div className={active ? "Menu__nav Menu__nav--active" : "Menu__nav"}>
+            <ul className="Menu__ul">
+              <li className={active ? "Menu__li Menu__li--active" : "Menu__li"}>
                 <NavLink
                   exact
                   to="/"
-                  activeClassName="Menu--link-selected"
-                  className="Menu--link"
+                  activeClassName="Menu__link--selected"
+                  className="Menu__link"
                   onClick={() => this.setState({ active: false })}
                 >
                   Home
                 </NavLink>
               </li>
-              <li className={active ? "Menu--li Menu--li-active" : "Menu--li"}>
+              <li className={active ? "Menu__li Menu__li--active" : "Menu__li"}>
                 <NavLink
                   to="/about"
-                  activeClassName="Menu--link-selected"
-                  className="Menu--link"
+                  activeClassName="Menu__link--selected"
+                  className="Menu__link"
                   onClick={() => this.setState({ active: false })}
                 >
                   About Me
                 </NavLink>
               </li>
-              <li className={active ? "Menu--li Menu--li-active" : "Menu--li"}>
+              <li className={active ? "Menu__li Menu__li--active" : "Menu__li"}>
                 <NavLink
                   to="/skills"
-                  activeClassName="Menu--link-selected"
-                  className="Menu--link"
+                  activeClassName="Menu__link--selected"
+                  className="Menu__link"
                   onClick={() => this.setState({ active: false })}
                 >
                   Services & Tech
                 </NavLink>
               </li>
-              <li className={active ? "Menu--li Menu--li-active" : "Menu--li"}>
+              <li className={active ? "Menu__li Menu__li--active" : "Menu__li"}>
                 <NavLink
                   to="/work"
-                  activeClassName="Menu--link-selected"
-                  className="Menu--link"
+                  activeClassName="Menu__link--selected"
+                  className="Menu__link"
                   onClick={() => this.setState({ active: false })}
                 >
                   My Work
                 </NavLink>
               </li>
-              <li className={active ? "Menu--li Menu--li-active" : "Menu--li"}>
+              <li className={active ? "Menu__li Menu__li--active" : "Menu__li"}>
                 <NavLink
                   to="/contact"
-                  activeClassName="Menu--link-selected"
-                  className="Menu--link"
+                  activeClassName="Menu__link--selected"
+                  className="Menu__link"
                   onClick={() => this.setState({ active: false })}
                 >
                   Contact Me
--- src/components/MenuButton/MenuButton.tsx
+++ src/components/MenuButton/MenuButton.tsx
@@ -9,32 +9,23 @@ interface IMenuStateProp {
 export default class MenuButton extends Component<IMenuStateProp> {
   public render() {
     const { menuState } = this.props;
+    const btnLine = (
+      <div
+        className={
+          menuState
+            ? "MenuButton__btnLine MenuButton__btnLine--active"
+            : "MenuButton__btnLine"
+        }
+      />
+    );
 
     return (
       <div
-        className={menuState ? "MenuButton MenuButton-active" : "MenuButton"}
+        className={menuState ? "MenuButton MenuButton--active" : "MenuButton"}
       >
-        <div
-          className={
-            menuState
-              ? "MenuButton--btnLine MenuButton--btnLine-active"
-              : "MenuButton--btnLine"
-          }
-        />
-        <div
-          className={
-            menuState
-              ? "MenuButton--btnLine MenuButton--btnLine-active"
-              : "MenuButton--btnLine"
-          }
-        />
-        <div
-          className={
-            menuState
-              ? "MenuButton--btnLine MenuButton--btnLine-active"
-              : "MenuButton--btnLine"
-          }
-        />
+        {btnLine}
+        {btnLine}
+        {btnLine}
       </div>
     );
   }
