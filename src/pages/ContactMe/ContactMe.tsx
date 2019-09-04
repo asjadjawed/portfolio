365089809e0e78d7ea94fa45e80a73af9a1a5e5e
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2019-09-04 15:26:48 +0500

    BEM + styling fix

--- src/pages/ContactMe/ContactMe.tsx
+++ src/pages/ContactMe/ContactMe.tsx
@@ -5,28 +5,28 @@ import "./ContactMe.scss";
 export default function ContactMe() {
   return (
     <div className="ContactMe">
-      <h1 className="ContactMe--heading">
-        Contact <span className="ContactMe--text-secondaryColor">Me</span>
+      <h1 className="ContactMe__heading">
+        Contact <span className="ContactMe__text--secondaryColor">Me</span>
       </h1>
-      <h2 className="ContactMe--subHeading">You can reach me via...</h2>
-      <div className="ContactMe--main">
-        <div className="ContactMe--contact">
+      <h2 className="ContactMe__subHeading">You can reach me via...</h2>
+      <div className="ContactMe__main">
+        <div className="ContactMe__contact">
           <h3>
-            <span className="ContactMe--text-secondaryColor">Email: </span>
+            <span className="ContactMe__text--secondaryColor">Email: </span>
             &#109;&#101;&#64;&#97;&#115;&#106;&#97;&#100;&#46;&#100;&#101;&#118;
           </h3>
         </div>
 
-        <div className="ContactMe--contact">
+        <div className="ContactMe__contact">
           <h3>
-            <span className="ContactMe--text-secondaryColor">Skype: </span>
+            <span className="ContactMe__text--secondaryColor">Skype: </span>
             asjadjawed
           </h3>
         </div>
 
-        <div className="ContactMe--contact">
+        <div className="ContactMe__contact">
           <h3>
-            <span className="ContactMe--text-secondaryColor">Twitter: </span>
+            <span className="ContactMe__text--secondaryColor">Twitter: </span>
             @asjadjawed
           </h3>
         </div>
