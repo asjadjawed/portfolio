7efee21a4c384fee61488e6eaf429dd8f9019ade
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2019-09-04 20:17:29 +0500

    BEM + style update

--- src/components/TechIcon/TechIcon.tsx
+++ src/components/TechIcon/TechIcon.tsx
@@ -10,18 +10,18 @@ interface ITechIconProps {
 
 const TechIcon: React.SFC<ITechIconProps> = ({ name, url, image }) => {
   return (
-    <div className={`TechIcon TechIcon--${name}`}>
+    <div className={`TechIcon TechIcon__${name}`}>
       <a
-        className="TechIcon--link"
+        className="TechIcon__link"
         href={url}
         target="_blank"
         rel="noopener noreferrer"
       >
-        <div className="TechIcon--icon">
+        <div className="TechIcon__icon">
           <img src={image} alt={name} title={name} style={{ width: "100%" }} />
         </div>
       </a>
-      <small className="TechIcon--subtitle">{name}</small>
+      <small className="TechIcon__subtitle">{name}</small>
     </div>
   );
 };
